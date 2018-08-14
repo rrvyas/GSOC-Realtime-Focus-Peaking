dc44ab4d070636140b12c42d6c803f91f026aba8
Rahul Vyas <37693464+rrvyas@users.noreply.github.com>
2018-08-14 12:32:59 +0200

    Update sobel_test.vhd

--- sobel_test.vhd
+++ sobel_test.vhd
@@ -1,3 +1,19 @@
+----------------------------------------------------------------------------------
+-- Company: APERTUS (GOOGLE SUMMER OF CODE - 2018)
+-- Engineer: RAHUL VYAS
+-- 
+-- Create Date:    00:50:43 06/11/2018 
+-- Design Name:    Sobel kernel test bench 
+-- Module Name:    sobel kernel test bench - Behavioral 
+-- Project Name:   REALTIME FOCUS PEAKING
+-- Target Devices: ZYNQ(MICROZED)
+-- Description : This VHDL module simulates the implementation of peaking algorithm on the incoming window of pixels.
+-----------------------------------------------------------------------------------
+-- This program is free software: you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation, either version
+-- 2 of the License, or (at your option) any later version.
+----------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
  
@@ -24,7 +40,6 @@ ARCHITECTURE behavior OF sobel_tb IS
          P_7 : IN  std_logic_vector(67 downto 0);
          P_8 : IN  std_logic_vector(67 downto 0);
 			clk : in std_logic;
-		--	valid_in : in std_logic;
 			valid_out : out std_logic;
          data_out : OUT  std_logic_vector(64 downto 0)
         );
@@ -41,22 +56,7 @@ ARCHITECTURE behavior OF sobel_tb IS
    signal P_6 : std_logic_vector(67 downto 0) := (others => '0');
    signal P_7 : std_logic_vector(67 downto 0) := (others => '0');
    signal P_8 : std_logic_vector(67 downto 0) := (others => '0');
---	signal grscl_0 : std_logic_vector (11 downto 0) := (others => '0');
---	signal grscl_1 : std_logic_vector (11 downto 0) := (others => '0');
---	signal grscl_2 : std_logic_vector (11 downto 0) := (others => '0');
---	signal grscl_3 : std_logic_vector (11 downto 0) := (others => '0');
---	signal grscl_4 : std_logic_vector (11 downto 0) := (others => '0');
---	signal grscl_5 : std_logic_vector (11 downto 0) := (others => '0');
---	signal grscl_6 : std_logic_vector (11 downto 0) := (others => '0');
---	signal grscl_7 : std_logic_vector (11 downto 0) := (others => '0');
---	signal grscl_8 : std_logic_vector (11 downto 0) := (others => '0');
---	signal valid_in : std_logic := '0';
-	signal valid_out : std_logic;
-
-
-
-
-
+   signal valid_out : std_logic;
  	--Outputs
    signal data_out : std_logic_vector(64 downto 0);
    -- No clocks detected in port list. Replace clk below with 
@@ -65,7 +65,6 @@ ARCHITECTURE behavior OF sobel_tb IS
    constant clk_period : time := 5 ns; -- 30 Mhz
  
 BEGIN
- 
 	-- Instantiate the Unit Under Test (UUT)
    uut: sobel_kernel PORT MAP (
           P_0 => P_0,
@@ -77,9 +76,8 @@ BEGIN
           P_6 => P_6,
           P_7 => P_7,
           P_8 => P_8,
-			 clk => clk,
-			-- valid_in => valid_in,
-			 valid_out => valid_out,
+	  clk => clk,
+          valid_out => valid_out,
           data_out => data_out
         );
 
@@ -96,42 +94,20 @@ BEGIN
    -- Stimulus process
    stim_proc: process
    begin		
-      -- hold reset state for 100 ns.
-      --wait for 100 ns;	
 
       wait for clk_period*10;
---valid_in <= '1';
-wait for clk_period;
-P_0<= "00000000000000000000000000100000000000000000000000000000000000011001";
-P_1<= "00000000000000000000000001100000000000000000000000000000000000001001";
-P_2<= "00000000000000000000000011100000000000000000000000000000000000001001";
-P_3<= "00000000000000000000000111100000000000000000000000000000000000001001";
-P_4<= "00000000000000000000000000100000000000000000000000000000000000001001";
-P_5<= "00000000000000000000000001100000000000000000000000000000000000001001";
-P_6<= "00000000000000000000000011100000000000000000000000000000000000001001";
-P_7<= "00000000000000000000000111100000000000000000000000000000000000001001";
-P_8<= "00000000000000000000000000100000000000000000000000000000000000001001";
-wait for clk_period;
---valid_in <= '0';
-
-
-      -- insert stimulus here 
-
+	wait for clk_period;
+	P_0<= "00000000000000000000000000100000000000000000000000000000000000011001";
+	P_1<= "00000000000000000000000001100000000000000000000000000000000000001001";
+	P_2<= "00000000000000000000000011100000000000000000000000000000000000001001";
+	P_3<= "00000000000000000000000111100000000000000000000000000000000000001001";
+	P_4<= "00000000000000000000000000100000000000000000000000000000000000001001";
+	P_5<= "00000000000000000000000001100000000000000000000000000000000000001001";
+	P_6<= "00000000000000000000000011100000000000000000000000000000000000001001";
+	P_7<= "00000000000000000000000111100000000000000000000000000000000000001001";
+	P_8<= "00000000000000000000000000100000000000000000000000000000000000001001";
+	wait for clk_period;
       wait;
    end process;
-
---g_process: process
---begin
---grscl_0 <= P_0(47 downto 36); 
---grscl_1 <= P_1(47 downto 36); 
---grscl_2 <= P_2(47 downto 36); 
---grscl_3 <= P_3(47 downto 36); 
---grscl_4 <= P_4(47 downto 36); 
---grscl_5 <= P_5(47 downto 36); 
---grscl_6 <= P_6(47 downto 36); 
---grscl_7 <= P_7(47 downto 36); 
---grscl_8 <= P_8(47 downto 36); 
--- wait;
---end process;
 
 END;
